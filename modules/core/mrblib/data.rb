12b366a71829d0a26ebe67de42344ff7617cc235
Ramiro Rojo <ramiro.rojo.cretta@gmail.com>
2020-08-09 18:08:04 -0300

    Make save data distint from other files

--- modules/core/mrblib/data.rb
+++ modules/core/mrblib/data.rb
@@ -1,12 +1,16 @@
 module Kernel
   module_function
 
-  def load_data(filename)
+  def load_file(filename)
     obj = nil
     # rubocop:disable Security/MarshalLoad
     File.open(filename) { |f| obj = Marshal.load(f) }
     # rubocop:enable Security/MarshalLoad
     obj
+  end
+
+  def load_data(filename)
+    load_data("@saves/#{filename}")
   end
 
   def save_data(obj, filename)
